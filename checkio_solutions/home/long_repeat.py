ef376c244f9b4d118860423958ce79fe7207e8c1
seanmcerlean82@gmail.com <bushido#1>
2017-08-15 22:08:11 +0100

    long_repeat take 2

--- checkio_solutions/home/long_repeat.py
+++ checkio_solutions/home/long_repeat.py
@@ -6,15 +6,13 @@ def long_repeat(line):
         length the longest substring that consists of the same char
     """
 
-
     if not line:
         return 0
 
-    longest_letter_sequences = [
-        len(max(re.findall('{0}+'.format(letter), line))) for letter in set(line)
-    ]
-
-    return max(longest_letter_sequences)
+    chars = set(line)
+    single_char_sequences = re.findall(r"{}+".format("+|".join(chars)), line)
+    single_char_sequence_lengths = map(len, single_char_sequences)
+    return max(single_char_sequence_lengths)
 
 
 if __name__ == '__main__':
